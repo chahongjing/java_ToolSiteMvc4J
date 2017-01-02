a257e319e45083b13e033f667eaf77d55aff50a9
chahongjing <310510906@qq.com>
2017-01-02 11:51:24 +0800

    配置文件修改

--- web/src/test/java/com/zjy/common/BaseTestCase.java
+++ web/src/test/java/com/zjy/common/BaseTestCase.java
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * @create 2016-12-15 20:52
  */
 @RunWith(SpringJUnit4ClassRunner.class)  //使用junit4进行测试
-@ContextConfiguration({"/applicationContext.xml", "/beans/spring-*.xml"}) //加载配置文件
+@ContextConfiguration({"/applicationContext.xml", "/config/spring-*.xml"}) //加载配置文件
 //------------如果加入以下代码，所有继承该类的测试类都会遵循该配置，也可以不加，在测试类的方法上///控制事务，参见下一个实例
 //这个非常关键，如果不加入这个注解配置，事务控制就会完全失效！
 //@Transactional
