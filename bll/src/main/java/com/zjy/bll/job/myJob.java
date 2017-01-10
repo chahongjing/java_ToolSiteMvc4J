cf72100e17c8a72ed7d17cdca5e6ea363e553953
chahongjing <310510906@qq.com>
2017-01-10 16:42:17 +0800

    job优化

--- bll/src/main/java/com/zjy/bll/job/myJob.java
+++ bll/src/main/java/com/zjy/bll/job/myJob.java
@@ -1,13 +1,28 @@
 package com.zjy.bll.job;
 
+import com.alibaba.fastjson.JSON;
+import com.zjy.bll.service.UserInfoService;
+import com.zjy.entities.UserInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by jyzeng on 2016/12/20.
  */
 public class myJob {
+    @Autowired
+    private UserInfoService userInfoService;
     private int counter = 0;
-    public void execute()  {
-        System.out.format("%1$tF %1$tT (第%2$d次执行)\n", new Date(), counter++);
+
+    public void execute() {
+        try {
+            System.out.format("%1$tF %1$tT (第%2$d次执行), userInfoService == null: %3$s\n", new Date(), counter++, String.valueOf(userInfoService == null));
+            List<UserInfo> list = userInfoService.query(new UserInfo());
+            System.out.println("list: " + JSON.toJSONString(list));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
