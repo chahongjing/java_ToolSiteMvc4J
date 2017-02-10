1a8a17547af39decb6397c659d17fc30b1be9fa9
chahongjing <310510906@qq.com>
2017-02-10 15:18:26 +0800

    xml完善

--- baseframework/src/main/java/com/zjy/baseframework/XmlHelper.java
+++ baseframework/src/main/java/com/zjy/baseframework/XmlHelper.java
@@ -1,10 +1,42 @@
 package com.zjy.baseframework;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
 import java.beans.XMLDecoder;
 import java.beans.XMLEncoder;
 import java.io.*;
 
 public class XmlHelper {
+
+    public static void toXml(Object obj, String path) {
+        // @XmlRootElement(name = "User")
+        JAXBContext context = null;
+        try {
+            context = JAXBContext.newInstance(obj.getClass());
+            FileWriter sw = new FileWriter(path);
+            Marshaller marshaller = context.createMarshaller();
+            marshaller.marshal(obj, sw);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+    }
+    public static Object toObject(Class<?> clazz, String path) {
+        JAXBContext context = null;
+        try {
+            context = JAXBContext.newInstance(clazz);
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+            Object obj = unmarshaller.unmarshal(new File(path));
+            return obj;
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 
     public static String serialize(Object o) {
         ByteArrayOutputStream stream = new ByteArrayOutputStream();
