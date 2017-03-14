67e7a8064466d56478d195ff4891605369c54efd
chahongjing <310510906@qq.com>
2017-03-14 21:31:52 +0800

    添加服务类

---
+++ baseframework/src/main/java/com/zjy/baseframework/Utils.java
@@ -0,0 +1,14 @@
+package com.zjy.baseframework;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.nio.file.Paths;
+
+/**
+ * Created by chahongjing on 2017/3/14.
+ */
+public class Utils {
+    public static String getRootPath() {
+        return Paths.get(StringUtils.removeStart(Utils.class.getClassLoader().getResource("/").getFile(), "/")).getParent().getParent().toAbsolutePath().toString();
+    }
+}
