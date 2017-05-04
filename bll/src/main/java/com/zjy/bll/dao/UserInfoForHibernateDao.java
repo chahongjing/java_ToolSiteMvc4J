647dedd5c9492728c2989ff3a5325f4cf0abb469
chahongjing <310510906@qq.com>
2017-05-05 00:20:19 +0800

    集成hibernate

---
+++ bll/src/main/java/com/zjy/bll/dao/UserInfoForHibernateDao.java
@@ -0,0 +1,32 @@
+package com.zjy.bll.dao;
+
+import com.zjy.entities.UserInfoForHibernate;
+//import org.hibernate.Session;
+//import org.hibernate.SessionFactory;
+//import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+/**
+ * Created by chahongjing on 2017/5/5.
+ */
+@Repository
+public class UserInfoForHibernateDao {
+//    @Autowired
+//    private SessionFactory sessionFactory;
+//
+//    public void test() {
+//        //下面开始我们的数据库操作
+//        Session session = sessionFactory.openSession();//从会话工厂获取一个session
+//        Transaction transaction = session.beginTransaction();//开启一个新的事务
+//
+//        UserInfoForHibernate user = new UserInfoForHibernate();
+//        user.setUserName("hello spring");
+//        user.setUserGuid(UUID.randomUUID().toString());
+//        session.save(user);
+//
+//        transaction.commit();//提交事务
+//    }
+}
--- web/src/main/java/com/zjy/web/controller/IndexController.java
+++ web/src/main/java/com/zjy/web/controller/IndexController.java
@@ -1,12 +1,18 @@
 package com.zjy.web.controller;
 
+import com.zjy.baseframework.ExcelHelper;
+import com.zjy.bll.dao.UserInfoForHibernateDao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * @author chahongjing
@@ -16,11 +22,49 @@ import javax.servlet.http.HttpServletResponse;
 public class IndexController {
     private Logger logger = LoggerFactory.getLogger(IndexController.class);
 
+    @Autowired
+    private UserInfoForHibernateDao userInfoForHibernateDao;
+
     @RequestMapping("/index.do")
     public String test(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        ExcelHelper<String> e = new ExcelHelper<>();
+
         return "OK";
     }
 
     public static void main(String[] args) {
+        Set<String> s = new LinkedHashSet<>();
+        s.add("b");
+        s.add("a");
+        s.add("d");
+        s.add("c");
+        s.add("d");
+        System.out.println(s);
+
+        Set<String> ts = new TreeSet<>();
+        ts.add("b");
+        ts.add("a");
+        ts.add("d");
+        ts.add("c");
+        ts.add("d");
+        System.out.println(ts);
+
+        Map<String, Integer> lhm = new LinkedHashMap<>();
+        lhm.put("b", 1);
+        lhm.put("a", 2);
+        lhm.put("d", 3);
+        lhm.put("c", 4);
+        lhm.put("d", 5);
+        System.out.println(lhm);
+
+        TreeMap<String, Integer> tm = new TreeMap<>();
+        tm.put("b", 1);
+        tm.put("a", 2);
+        tm.put("d", 3);
+        tm.put("c", 4);
+        tm.put("d", 5);
+        System.out.println(tm);
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
     }
 }
