0c2b24c9f9b547c1cb55443c696bfad2994d73d2
chahongjing <310510906@qq.com>
2018-01-08 10:07:13 +0800

    框架更新

--- baseframework/src/main/java/com/zjy/baseframework/DbHelperNew.java
+++ baseframework/src/main/java/com/zjy/baseframework/DbHelperNew.java
@@ -17,11 +17,11 @@ import java.util.List;
 public class DbHelperNew {
     private static DataSource dataSource;
 
-    static{
+    static {
         dataSource = SpringContextHolder.getBean("dataSourceC3p0");
         // 如果要使用c3p0连接池，则要使用QueryRunner qr = new QueryRunner(dataSource);
         String driver = PropertiesHelper.getInstance().getProperties("db.driverClassName");
-        if(!DbUtils.loadDriver(driver)) {
+        if (!DbUtils.loadDriver(driver)) {
             System.out.println("加载数据库驱动失败！");
         }
     }
@@ -53,20 +53,20 @@ public class DbHelperNew {
     public static <T> T get(String sql, Class<T> clazz) {
 
         List<T> list = getList(sql, clazz);
-        if(list != null && !list.isEmpty()) {
+        if (list != null && !list.isEmpty()) {
             return list.get(0);
         } else {
             return null;
         }
     }
 
-    public static <T> List<T> getList(String sql, Class<T> clazz) {
+    public static <T> List<T> getList(String sql, Class<T> clazz, Object... params) {
         Connection conn = null;
         List<T> list = null;
         try {
             conn = getConnection();
             QueryRunner qr = new QueryRunner();
-            list = (List<T>)qr.query(conn, sql, new BeanListHandler(clazz));
+            list = (List<T>) qr.query(conn, sql, new BeanListHandler(clazz), params);
 
             // 使用c3p0连接池
 //            conn = getConnection();
@@ -83,6 +83,7 @@ public class DbHelperNew {
 
     /**
      * 获取数据库连接，也可以使用c3p0连接池，通过dataSource.getConnection()来获取
+     *
      * @return
      */
     public static Connection getConnection() {
