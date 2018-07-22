71389f0199dc25fe52152a57331dc58f507da5c9
chahongjing <310510906@qq.com>
2018-07-22 16:19:38 +0800

    完善

---
+++ web/mybatis-generator.bat
@@ -0,0 +1,9 @@
+rem 先在tomcat-users.xml文件中添加 <role rolename="manager-gui"/><role rolename="manager-script" /><user username="admin" password="admin" roles="manager-script,admin-gui" />
+rem 再到mvn settings.xml文件中配置<server><id>tomcat7</id><username>admin</username><password>admin</password></server>
+@echo off
+title mybatis-generator
+set command=mvn mybatis-generator:generate
+echo.
+echo %command%
+%command%
+pause;
