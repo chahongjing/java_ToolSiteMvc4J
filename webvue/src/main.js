a8420f18e8db96256062736d43f5422352f0ac1b
chahongjing <310510906@qq.com>
2018-11-01 08:49:44 +0800

    微调

--- webvue/src/main.js
+++ webvue/src/main.js
@@ -14,6 +14,7 @@ Vue.prototype.qs = Qs;
 
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8';
 axios.defaults.headers.common["x-requested-with"] = "XMLHttpRequest";
+axios.defaults.withCredentials = true;
 axios.defaults.paramsSerializer = function(params) {
     return $.param(params);
 };
