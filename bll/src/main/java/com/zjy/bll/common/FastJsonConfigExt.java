e10a544e884bc94420f2595ff1ad36b210820c08
chahongjing <310510906@qq.com>
2018-11-15 17:32:59 +0800

    优化

---
+++ bll/src/main/java/com/zjy/bll/common/FastJsonConfigExt.java
@@ -0,0 +1,23 @@
+package com.zjy.bll.common;
+
+import com.alibaba.fastjson.serializer.SerializeConfig;
+import com.alibaba.fastjson.serializer.ToStringSerializer;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+
+import java.math.BigInteger;
+import java.util.Date;
+
+/**
+ * Created by Administrator on 2018/11/15.
+ */
+public class FastJsonConfigExt extends FastJsonConfig {
+    public FastJsonConfigExt() {
+        super();
+        SerializeConfig serializeConfig = SerializeConfig.globalInstance;
+        serializeConfig.put(BigInteger.class, ToStringSerializer.instance);
+        serializeConfig.put(Long.class, ToStringSerializer.instance);
+        serializeConfig.put(Long.TYPE, ToStringSerializer.instance);
+        serializeConfig.put(Date.class, new DateFormaterFilter());
+        this.setSerializeConfig(serializeConfig);
+    }
+}
