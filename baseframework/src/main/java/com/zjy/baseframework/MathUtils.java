1b9b9878249d854446a69c15bb573e32d105a3ad
chahongjing <310510906@qq.com>
2018-12-09 09:54:06 +0800

    完善

--- baseframework/src/main/java/com/zjy/baseframework/MathUtils.java
+++ baseframework/src/main/java/com/zjy/baseframework/MathUtils.java
@@ -2,7 +2,6 @@ package com.zjy.baseframework;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.util.Objects;
 
 public class MathUtils {
     // 默认除法运算精度
@@ -15,12 +14,8 @@ public class MathUtils {
      * @param value2 加数
      * @return 两个参数的和
      */
-    public static <T extends Number> double add(T value1, T value2) {
-        Objects.requireNonNull(value1);
-        Objects.requireNonNull(value2);
-        BigDecimal b1 = new BigDecimal(value1.doubleValue());
-        BigDecimal b2 = new BigDecimal(value2.doubleValue());
-        return b1.add(b2).doubleValue();
+    public static float add(float value1, float value2) {
+        return (float) add((double) value1, (double) value2);
     }
 
     /**
@@ -30,12 +25,8 @@ public class MathUtils {
      * @param value2 减数
      * @return 两个参数的差
      */
-    public static <T extends Number> double sub(T value1, T value2) {
-        Objects.requireNonNull(value1);
-        Objects.requireNonNull(value2);
-        BigDecimal b1 = new BigDecimal(value1.doubleValue());
-        BigDecimal b2 = new BigDecimal(value2.doubleValue());
-        return b1.subtract(b2).doubleValue();
+    public static float sub(float value1, float value2) {
+        return (float) sub((double) value1, (double) value2);
     }
 
     /**
@@ -45,12 +36,8 @@ public class MathUtils {
      * @param value2 乘数
      * @return 两个参数的积
      */
-    public static <T extends Number> double mul(T value1, T value2) {
-        Objects.requireNonNull(value1);
-        Objects.requireNonNull(value2);
-        BigDecimal b1 = new BigDecimal(value1.doubleValue());
-        BigDecimal b2 = new BigDecimal(value2.doubleValue());
-        return b1.multiply(b2).doubleValue();
+    public static float mul(float value1, float value2) {
+        return (float) mul((double) value1, (double) value2);
     }
 
     /**
@@ -60,7 +47,7 @@ public class MathUtils {
      * @param divisor  除数
      * @return 两个参数的商
      */
-    public static <T extends Number> double divide(T dividend, T divisor) {
+    public static float divide(float dividend, float divisor) {
         return divide(dividend, divisor, DEF_DIV_SCALE);
     }
 
@@ -72,14 +59,85 @@ public class MathUtils {
      * @param scale    表示表示需要精确到小数点以后几位。
      * @return 两个参数的商
      */
-    public static <T extends Number> double divide(T dividend, T divisor, int scale) {
-        Objects.requireNonNull(dividend);
-        Objects.requireNonNull(divisor);
+    public static float divide(float dividend, float divisor, int scale) {
+        return (float) divide((double) dividend, (double) divisor, scale);
+    }
+
+    /**
+     * 提供指定数值的（精确）小数位四舍五入处理。
+     *
+     * @param value 需要四舍五入的数字
+     * @param scale 小数点后保留几位
+     * @return 四舍五入后的结果
+     */
+    public static float round(float value, int scale) {
+        return (float) round((double) value, scale);
+    }
+
+    /**
+     * 提供精确的加法运算。
+     *
+     * @param value1 被加数
+     * @param value2 加数
+     * @return 两个参数的和
+     */
+    public static double add(double value1, double value2) {
+        BigDecimal b1 = new BigDecimal(value1);
+        BigDecimal b2 = new BigDecimal(value2);
+        return b1.add(b2).doubleValue();
+    }
+
+    /**
+     * 提供精确的减法运算。
+     *
+     * @param value1 被减数
+     * @param value2 减数
+     * @return 两个参数的差
+     */
+    public static double sub(double value1, double value2) {
+        BigDecimal b1 = new BigDecimal(value1);
+        BigDecimal b2 = new BigDecimal(value2);
+        return b1.subtract(b2).doubleValue();
+    }
+
+    /**
+     * 提供精确的乘法运算。
+     *
+     * @param value1 被乘数
+     * @param value2 乘数
+     * @return 两个参数的积
+     */
+    public static <T extends Number> double mul(double value1, double value2) {
+        BigDecimal b1 = new BigDecimal(value1);
+        BigDecimal b2 = new BigDecimal(value2);
+        return b1.multiply(b2).doubleValue();
+    }
+
+    /**
+     * 提供（相对）精确的除法运算，当发生除不尽的情况时， 精确到小数点以后10位，以后的数字四舍五入。
+     *
+     * @param dividend 被除数
+     * @param divisor  除数
+     * @return 两个参数的商
+     */
+    public static <T extends Number> double divide(double dividend, double divisor) {
+        return divide(dividend, divisor, DEF_DIV_SCALE);
+    }
+
+    /**
+     * 提供（相对）精确的除法运算。 当发生除不尽的情况时，由scale参数指定精度，以后的数字四舍五入。
+     *
+     * @param dividend 被除数
+     * @param divisor  除数
+     * @param scale    表示表示需要精确到小数点以后几位。
+     * @return 两个参数的商
+     */
+    public static <T extends Number> double divide(double dividend, double divisor, int scale) {
         if (scale < 0) {
             throw new IllegalArgumentException("The scale must be a positive integer or zero");
         }
-        BigDecimal b1 = new BigDecimal(dividend.doubleValue());
-        BigDecimal b2 = new BigDecimal(divisor.doubleValue());
+        BigDecimal b1 = new BigDecimal(dividend);
+        BigDecimal b2 = new BigDecimal(divisor);
         return b1.divide(b2, scale, RoundingMode.HALF_UP).doubleValue();
     }
 
@@ -90,12 +148,11 @@ public class MathUtils {
      * @param scale 小数点后保留几位
      * @return 四舍五入后的结果
      */
-    public static <T extends Number> double round(T value, int scale) {
-        Objects.requireNonNull(value);
+    public static <T extends Number> double round(double value, int scale) {
         if (scale < 0) {
             throw new IllegalArgumentException("The scale must be a positive integer or zero");
         }
-        BigDecimal b = new BigDecimal(value.doubleValue());
+        BigDecimal b = new BigDecimal(value);
         b.setScale(scale, RoundingMode.HALF_UP);
         return b.doubleValue();
     }
