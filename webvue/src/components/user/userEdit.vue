97f7cf9af2defea416333008d238871c97c95f49
chahongjing <310510906@qq.com>
2019-01-02 12:02:49 +0800

    按钮添加动画效果

--- webvue/src/components/user/userEdit.vue
+++ webvue/src/components/user/userEdit.vue
@@ -129,7 +129,7 @@
 
         <div class="form-group text-right mb0">
           <button type="button" class="btn btn-outline-purple" @click='$root.goBack()'>
-            <i class='fa fa-arrow-circle-o-left fa-plus-myrotate'></i><span>返回</span>
+            <i class='fa fa-arrow-circle-o-left fa-plus-myrotate fa-back-myrotate'></i><span>返回</span>
           </button>
           <button type="button" class="btn btn-purple mr5" @click="save" :disabled='allDisabled'>
             <i class='fa fa-save'></i>保存
