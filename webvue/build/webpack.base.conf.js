aa9da6cd1b3701ba45c68b42678e2399698749dc
chahongjing <310510906@qq.com>
2019-01-08 16:49:29 +0800

    添加ie支持

--- webvue/build/webpack.base.conf.js
+++ webvue/build/webpack.base.conf.js
@@ -4,6 +4,8 @@ const utils = require('./utils')
 const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
 
+require('babel-polyfill')
+
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
 }
@@ -13,7 +15,8 @@ function resolve (dir) {
 module.exports = {
   context: path.resolve(__dirname, '../'),
   entry: {
-    app: './src/main.js'
+    // app: './src/main.js',
+    app: ['babel-polyfill', './src/main.js']
   },
   output: {
     path: config.build.assetsRoot,
