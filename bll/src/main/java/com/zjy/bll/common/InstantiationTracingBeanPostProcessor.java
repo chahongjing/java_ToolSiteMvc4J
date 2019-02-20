1b5b59f4c9d3288215259ba7290b9c441513f50a
chahongjing <310510906@qq.com>
2019-02-20 15:03:48 +0800

    完善spring事件

--- bll/src/main/java/com/zjy/bll/common/InstantiationTracingBeanPostProcessor.java
+++ bll/src/main/java/com/zjy/bll/common/InstantiationTracingBeanPostProcessor.java
@@ -5,8 +5,12 @@ import com.zjy.baseframework.ReflectionHelper;
 import com.zjy.baseframework.mybatis.CodeEnumTypeHandler;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextClosedEvent;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.ContextStartedEvent;
+import org.springframework.context.event.ContextStoppedEvent;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -15,7 +19,7 @@ import java.util.List;
  * Created by Administrator on 2018/5/16.
  */
 @Component
-public class InstantiationTracingBeanPostProcessor implements ApplicationListener<ContextRefreshedEvent> {
+public class InstantiationTracingBeanPostProcessor implements ApplicationListener<ApplicationEvent> {
 
     @Autowired
     private SqlSessionFactoryBean sqlSessionFactory;
@@ -26,10 +30,13 @@ public class InstantiationTracingBeanPostProcessor implements ApplicationListene
      * @param event
      */
     @Override
-    public void onApplicationEvent(ContextRefreshedEvent event) {
+    public void onApplicationEvent(ApplicationEvent event) {
+        //root application context 没有parent，他就是老大.
+        if (((ContextRefreshedEvent) event).getApplicationContext().getParent() != null) return;
         //需要执行的逻辑代码，当spring容器初始化完成后就会执行该方法。
-        if (event.getApplicationContext().getParent() == null) {
-            //root application context 没有parent，他就是老大.
+        if (event instanceof ContextClosedEvent) {
+            System.out.println(event.getClass().getSimpleName() + " 事件已发生！");
+        } else if (event instanceof ContextRefreshedEvent) {
             // 需要执行的逻辑代码，当spring容器初始化完成后就会执行该方法。
             // 获取所有类型
             List<Class> classList = ReflectionHelper.getProjectClassList();
@@ -42,6 +49,13 @@ public class InstantiationTracingBeanPostProcessor implements ApplicationListene
             }
             // 初始化要序列化的枚举
             EnumHelper.initAllSerializeEnum(classList);
+            System.out.println(event.getClass().getSimpleName() + " 事件已发生！");
+        } else if (event instanceof ContextStartedEvent) {
+            System.out.println(event.getClass().getSimpleName() + " 事件已发生！");
+        } else if (event instanceof ContextStoppedEvent) {
+            System.out.println(event.getClass().getSimpleName() + " 事件已发生！");
+        } else {
+            System.out.println("有其它事件发生:" + event.getClass().getName());
         }
     }
 }
