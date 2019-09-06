a2300466f50ba94b49f91eab0a2dba68381d96d7
junyi.zeng <junyi.zeng@dmall.com>
2019-09-06 09:02:02 +0800

    去掉无用的文件

--- baseframework/src/main/java/com/zjy/baseframework/CommonReg.java
+++
@@ -1,8 +0,0 @@
-package com.zjy.baseframework;
-
-import java.util.regex.Pattern;
-
-public class CommonReg {
-    // 中文，字母，数字，/和-
-    public static Pattern aa = Pattern.compile("^[\\u4e00-\\u9fa5_a-zA-Z0-9/\\-]*$");
-}
