98c9a458c33d504d9719680ea1a142a8b1dd4a5c
zengjunyi <zengjunyi@xiaomi.com>
2022-03-31 19:49:25 +0800

    feat: 添加开关控制页面

--- webvue/src/components/switch/list.vue
+++ webvue/src/components/switch/list.vue
@@ -5,9 +5,9 @@
         <label class="form-label colon" v-text="item.name"></label>
         <div class="form-content">
           <label class="togglecheckbox">
-            <input type='checkbox' :name="item.key" v-model="item.value" @change="changeSwitch(item)" :disabled="allDisabled"/>
+            <input type='checkbox' :name="item.key" true-value="1" false-value="0" v-model="item.value" @change="changeSwitch(item)" :disabled="allDisabled"/>
             <i></i>
-            <span>abcd</span>
+            <span></span>
           </label>
         </div>
       </div>
@@ -29,15 +29,18 @@ export default {
       var me = this;
       me.$axios.get('/switch/queryAllSwitchList').then(function (resp) {
         if(resp.data.status === ResultStatus.OK.key) {
-          me.switchList = resp.data.data;
+          me.switchList = resp.data.value;
+          me.allDisabled = false;
         }
       });
     },
     changeSwitch: function(item) {
       var me = this;
+      me.allDisabled = true;
       me.$axios.get('/switch/updateSwitch', {key: item.key, value: item.value}).then(function (resp) {
         if(resp.data.status === ResultStatus.OK.key) {
-          me.$toaster.success(resp.data.data);
+          me.$toaster.success(resp.data.value);
+          me.allDisabled = false;
         }
       });
     }
@@ -49,5 +52,5 @@ export default {
 </script>
 
 <style scoped>
-
+  .myform{width:200px;margin:0 auto;}
 </style>
