70f9820d283e0c63bc5fb35a7eebd69ea37ee423
zengjunyi <zengjunyi@xiaomi.com>
2022-05-12 16:24:41 +0800

    feat: 集成redis

---
+++ webvue/src/components/tools/redis.vue
@@ -0,0 +1,118 @@
+<template>
+  <div class='maincontent'>
+    <div class="mypanel" style="width:500px;margin:auto;margin-top:20px;">
+      <div class="panel-heading font-bold">用户信息</div>
+      <div class="panel-body">
+        <form class='myform infotip form-label-w110 block-form-group'>
+          <div class="form-group">
+            <label class="form-label req colon">数据类型</label>
+            <div class="form-content">
+              <select class='form-control' v-model="dataType">
+                <option v-for="item in dataTypeOption" :value="item.key" v-text="item.name"></option>
+              </select>
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="form-label req colon">操作类型</label>
+            <div class="form-content">
+              <select class='form-control' v-model="opType">
+                <option v-for="item in dataOpOption" :value="item.key" v-text="item.name"></option>
+              </select>
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="form-label req colon">键</label>
+            <div class="form-content">
+              <input type="text" class="form-control" placeholder="键"
+                     v-model='user.userCode' :disabled='allDisabled'/>
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="form-label req colon">字段</label>
+            <div class="form-content">
+              <input type="text" class="form-control" placeholder="字段"
+                     v-model='key' :disabled='allDisabled'/>
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="form-label req colon">值</label>
+            <div class="form-content">
+              <input type="text" class="form-control" placeholder="值"
+                     v-model='value' :disabled='allDisabled'/>
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="form-label req colon">分数</label>
+            <div class="form-content">
+              <input type="text" class="form-control" placeholder="分数"
+                     v-model='score' :disabled='allDisabled'/>
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="form-label req colon">分数</label>
+            <div class="form-content">
+               <textarea type="text" class="form-control" placeholder="说明"
+                         v-model='opResult' :disabled='allDisabled'></textarea>
+            </div>
+          </div>
+
+          <div class="form-group text-right mb0">
+            <button type="button" class="btn btn-purple mr5" @click="opRedis" :disabled='allDisabled'>
+              <i class='fa fa-save'></i>操作
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "redis",
+  data () {
+    return {
+      allDisabled: true,
+      dataType: 'STRING',
+      opType: 'GET',
+      key: '',
+      field: '',
+      value: '',
+      score: null,
+      dataTypeOption: window.enumMap['RedisDataType'],
+      dataOpOption: window.enumMap['RedisOpType'],
+      opResult: {}
+    }
+  },
+  methods: {
+    init: function () {
+
+    },
+    opRedis: function() {
+      var me = this;
+      me.allDisabled = true;
+      var param = {
+        dataType: this.dataType,
+        opType: this.opType,
+        key: this.key,
+        field: this.field,
+        value: this.value,
+        score: this.score
+      };
+      me.$axios.post('/redis/optRedis').then(function (resp) {
+        if(resp.data.status === ResultStatus.OK.key) {
+          me.opResult = resp.data.value;
+          me.allDisabled = false;
+        }
+      });
+    }
+  },
+  mounted: function () {
+    this.init();
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
